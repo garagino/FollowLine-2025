c0b165bfb7b35093e85579ec13c73075babac6e0
Ênio Bazante <85653598+EnioBazante@users.noreply.github.com>
2025-01-23 11:46:59 -0300

    Update BidirectionalEncoder.ino's new strategie

--- components/encoder/BidirectionalEncoder/BidirectionalEncoder.ino
+++ components/encoder/BidirectionalEncoder/BidirectionalEncoder.ino
@@ -1,85 +1,50 @@
+#include <Arduino.h>
 
+// Define encoder pins
+int encoderLeftPin1 = 33;   // Encoder Output 'A' for left encoder
+int encoderLeftPin2 = 32;   // Encoder Output 'B' for left encoder
+int encoderRightPin1 = 25;  // Encoder Output 'A' for left encoder
+int encoderRightPin2 = 26;  // Encoder Output 'B' for left encoder
 
+volatile long encoderValueRight = 0;   // Variable to store encoder count
+volatile long encoderValueLeft = 0;  // Variable to store encoder count
 
-// Encoder ports at Robocore's Vespa
-int encoderLeftPin1 = 25;
-int encoderLeftPin2 = 26; 
+// Interrupt service routine for encoder A
+void updateRightEncoder() {
+  if (digitalRead(encoderRightPin1) == digitalRead(encoderRightPin2)) {
+    encoderValueRight++;
+  } else {
+    encoderValueRight--;
+  }
+}
 
-int encoderRightPin1 = 33; 
-int encoderRightPin2 = 32; 
+void updateLeftEncoder(){
+  if (digitalRead(encoderLeftPin1) == digitalRead(encoderLeftPin2)) {
+  encoderValueLeft++;
+}
+else {
+  encoderValueLeft--;
+}
+}
 
-
-volatile int lastEncodedLeft = 0; // Here updated value of encoder store.
-volatile int lastEncodedRight = 0; // Here updated value of encoder store.
-
-volatile long encoderLeftValue = 0; // Raw encoder value
-volatile long encoderRightValue = 0; // Raw encoder value
-
-
-void setup(){
-
-
-  // Sets serial speed
+void setup() {
+  pinMode(encoderLeftPin1, INPUT);
+  pinMode(encoderLeftPin2, INPUT);
+  pinMode(encoderRightPin1, INPUT);
+  pinMode(encoderRightPin2, INPUT);
   Serial.begin(115200);
 
-  // Configures All encoders to PullUp 
-  pinMode(encoderLeftPin1, INPUT_PULLUP); 
-  pinMode(encoderLeftPin2, INPUT_PULLUP);
-
-  pinMode(encoderRightPin1, INPUT_PULLUP); 
-  pinMode(encoderRightPin2, INPUT_PULLUP);
-
-
-  //turn pullup resistor on
-  digitalWrite(encoderLeftPin1, HIGH);
-  digitalWrite(encoderLeftPin2, HIGH);
-
-  digitalWrite(encoderRightPin1, HIGH);
-  digitalWrite(encoderRightPin2, HIGH);
-
-
-
-  //call updateEncoder() when any high/low changed seen
-
-  attachInterrupt(encoderLeftPin1, updateEncoder, CHANGE);
-  attachInterrupt(encoderRightPin1, updateEncoder, CHANGE);
-  attachInterrupt(encoderLeftPin2, updateEncoder, CHANGE);
-  attachInterrupt(encoderRightPin2, updateEncoder, CHANGE);
-
+  // Attach interrupt for encoder pin A (interrupt 0 is pin 2 on UNO)
+  attachInterrupt(digitalPinToInterrupt(encoderLeftPin1), updateLeftEncoder, CHANGE);
+  attachInterrupt(digitalPinToInterrupt(encoderRightPin1), updateRightEncoder, CHANGE);
 }
 
-void loop(){
-    
-  Serial.print("Esquerda: " );
-  Serial.println(encoderLeftValue);
-  Serial.print("Direita: " );
-  Serial.println(encoderRightValue);
-    
-}
-
-void updateEncoder(){
-
-
-  int MSB = digitalRead(encoderLeftPin1); //MSB = most significant bit
-  int LSB = digitalRead(encoderLeftPin2); //LSB = least significant bit
-
-  int encoded = (MSB << 1) |LSB; //converting thex 2 pin value to single number
-  int sum  = (lastEncodedLeft << 2) | encoded; //adding it to the previous encoded value
-
-  if(sum == 0b1101 || sum == 0b0100 || sum == 0b0010 || sum == 0b1011) encoderLeftValue --;
-  if(sum == 0b1110 || sum == 0b0111 || sum == 0b0001 || sum == 0b1000) encoderLeftValue ++;
-
-  lastEncodedLeft = encoded; //store this value for next time
-
-  MSB = digitalRead(encoderRightPin1); //MSB = most significant bit
-  LSB = digitalRead(encoderRightPin2); //LSB = least significant bit
-
-  encoded = (MSB << 1) |LSB; //converting thex 2 pin value to single number
-  sum  = (lastEncodedRight << 2) | encoded; //adding it to the previous encoded value
-
-  if(sum == 0b1101 || sum == 0b0100 || sum == 0b0010 || sum == 0b1011) encoderRightValue --;
-  if(sum == 0b1110 || sum == 0b0111 || sum == 0b0001 || sum == 0b1000) encoderRightValue ++;
-
-  lastEncodedRight = encoded; //store this value for next time
-
+void loop() {
+  // Set motor direction and speed
+  // Do something with the encoder count
+  // For example, stop the motor after a certain number of counts
+  Serial.print("Esquerda: ");
+  Serial.print(encoderValueLeft);
+  Serial.print(" Direita: ");
+  Serial.println(encoderValueRight);
 }
